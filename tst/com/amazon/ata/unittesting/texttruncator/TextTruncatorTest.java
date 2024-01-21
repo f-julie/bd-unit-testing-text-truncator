83e294e9eae29c5c70c697f48ec2ab25b5a58712
Julie Fiess <juliefiess@gmail.com>
2024-01-20 17:14:50 -0700

    Submission for quiz

--- tst/com/amazon/ata/unittesting/texttruncator/TextTruncatorTest.java
+++ tst/com/amazon/ata/unittesting/texttruncator/TextTruncatorTest.java
@@ -23,5 +23,49 @@ public class TextTruncatorTest {
     }
 
     /* ADD YOUR TESTS HERE */
+    @Test
+    public void truncateTo_withNull_resultsInAnEmptyString() {
+        // GIVEN
+        TextTruncator truncator = new TextTruncator(null);
 
+        // WHEN
+        String truncatedString = truncator.truncateTo(10);
+
+        // THEN
+        Assertions.assertEquals("", truncatedString,
+                "Expected to truncate null to an empty string.");
+
+    }
+
+    @Test
+    public void truncateTo_withLongString_returnedLengthMatchesNumChars() {
+        // GIVEN
+        String longString = "This is a very long string that will be truncated.";
+        int numChars = 10;
+        TextTruncator truncator = new TextTruncator(longString);
+
+        // WHEN
+        String truncatedString = truncator.truncateTo(numChars);
+
+        // THEN
+        Assertions.assertEquals(numChars, truncatedString.length(),
+                "The length of the truncated string should match numChars.");
+    }
+
+    @Test
+    public void truncateTo_withShortString_returnedStringEqualsGivenString() {
+        // GIVEN
+        String shortString = "short";
+        int numChars = 10;
+        String suffix = "...";
+        TextTruncator truncator = new TextTruncator(shortString);
+
+        // WHEN
+        String truncatedString = truncator.truncateTo(numChars, suffix);
+
+        // THEN
+        Assertions.assertEquals(shortString, truncatedString,
+                "The returned string should match the original string when not truncated.");
+
+    }
 }
